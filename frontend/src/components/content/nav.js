e2329440232ee3221ce1e8bb75c2cc7ce3aa3d84
t-hasic <thasic@mit.edu>
2024-03-10 14:47:46 -0400

    made social media links open in new tab

--- frontend/src/components/content/nav.js
+++ frontend/src/components/content/nav.js
@@ -1,7 +1,7 @@
-import logo from "../../assets/svg/logo_dark.svg"
+import logo from "../../assets/svg/logo_dark.svg";
 
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 import { Link } from "@mui/material";
 import { SECTIONS, THEME } from "../../constants";
@@ -10,107 +10,126 @@ import { SECTIONS, THEME } from "../../constants";
 const DEFAULT = SECTIONS.HOME.TITLE;
 
 export const NAV_GC = (props) => {
-    const right = [];
-    const sections = [
-        {
-            name: SECTIONS.DONATE.TITLE,
-            href: "https://giving.mit.edu/form/#/"
-        },
-        {
-            name: SECTIONS.LIFE.TITLE,
-        },
-        {
-            inPage: true,
-            name: SECTIONS.MAILING_LIST.TITLE,
-        },
-        // {
-        //     name: SECTIONS.PROGRAMS.TITLE,
-        //     dropdown: true,
-        // },  
-    ];
-    for (const section of sections) {
-        right.push(
-            {
-                meta: {
-                    className: `public-sans`,
-                },
-                content: {
-                    title: 
-                    <Link
-                        href={section.href? section.href : `/${section.inPage? "#" : ""}${section.name}`}
-                        id={`nav-${section.name}`}
-                        style={{
-                            ...THEME.NAV.STYLE.BTN, 
-                            color: THEME.SECONDARY,
-                            borderRadius: "5px",
-                            margin: "0 20px 0 20px", 
-                        }}
-                        className={`${section.name === DEFAULT? "select-section" : ""}`}
-                    >
-                       {section.icon? <section.icon style={{margin: "0 10px 0 0"}}/> : <></>} {section.name.toUpperCase().replace("-", " ")} 
-                    </Link>
-                }
-            },
-        )
-    }
-    
-    const select = (section) => {
-        for (const section of sections) {
-            const sectionBtn = document.getElementById(`nav-${section.name}`);
-            if (sectionBtn) sectionBtn.classList.remove("select-section");
-        }
-
-        const sectionBtn = document.getElementById(section.id);
-        if (sectionBtn) {
-            props.setOpen(false);
-            props.setSelected(section.name);
-            sectionBtn.classList.add("select-section");
-        }
-    }
-    return {
-        NAV_LEFT: [
-            {
-                meta: {
-                    href: "/",
-                    id: `nav-${SECTIONS.HOME.TITLE}`,
-                    style: {margin: "0 0 10px 0", display: "flex", flexDirection: "column"},
-                },
-                content: {
-                    title: <img src={logo} height="50px" className="pointer align-self-center"/>
-                }
-            },
-            
-        ],
-
-        NAV_RIGHT: [
-            ...right,
-            {
-                meta: {
-                    isIcon: true,
-                    style: {margin: "0 10px 0 0px", height: "30px"},
-                    iconProps: {
-                        style: {fontSize: "18px"}
-                    },
-                    href: "https://www.instagram.com/mitmsa/", 
-                },
-                content: {
-                    icon: InstagramIcon,
-                }
-            },
-
-            {
-                meta: {
-                    isIcon: true,
-                    style: {margin: "0 0 0 20px", height: "30px"},
-                    iconProps: {
-                        style: {fontSize: "18px"}
-                    },
-                    href: "https://www.facebook.com/mitmsa", 
-                },
-                content: {
-                    icon: FacebookIcon,
-                }
+  const right = [];
+  const sections = [
+    {
+      name: SECTIONS.DONATE.TITLE,
+      href: "https://giving.mit.edu/form/#/",
+    },
+    {
+      name: SECTIONS.LIFE.TITLE,
+    },
+    {
+      inPage: true,
+      name: SECTIONS.MAILING_LIST.TITLE,
+    },
+    // {
+    //     name: SECTIONS.PROGRAMS.TITLE,
+    //     dropdown: true,
+    // },
+  ];
+  for (const section of sections) {
+    right.push({
+      meta: {
+        className: `public-sans`,
+      },
+      content: {
+        title: (
+          <Link
+            href={
+              section.href
+                ? section.href
+                : `/${section.inPage ? "#" : ""}${section.name}`
             }
-        ]
+            id={`nav-${section.name}`}
+            style={{
+              ...THEME.NAV.STYLE.BTN,
+              color: THEME.SECONDARY,
+              borderRadius: "5px",
+              margin: "0 20px 0 20px",
+            }}
+            className={`${section.name === DEFAULT ? "select-section" : ""}`}
+          >
+            {section.icon ? (
+              <section.icon style={{ margin: "0 10px 0 0" }} />
+            ) : (
+              <></>
+            )}{" "}
+            {section.name.toUpperCase().replace("-", " ")}
+          </Link>
+        ),
+      },
+    });
+  }
+
+  const select = (section) => {
+    for (const section of sections) {
+      const sectionBtn = document.getElementById(`nav-${section.name}`);
+      if (sectionBtn) sectionBtn.classList.remove("select-section");
     }
-}
+
+    const sectionBtn = document.getElementById(section.id);
+    if (sectionBtn) {
+      props.setOpen(false);
+      props.setSelected(section.name);
+      sectionBtn.classList.add("select-section");
+    }
+  };
+  return {
+    NAV_LEFT: [
+      {
+        meta: {
+          href: "/",
+          id: `nav-${SECTIONS.HOME.TITLE}`,
+          style: {
+            margin: "0 0 10px 0",
+            display: "flex",
+            flexDirection: "column",
+          },
+        },
+        content: {
+          title: (
+            <img
+              src={logo}
+              height="50px"
+              className="pointer align-self-center"
+            />
+          ),
+        },
+      },
+    ],
+
+    NAV_RIGHT: [
+      ...right,
+      {
+        meta: {
+          isIcon: true,
+          style: { margin: "0 10px 0 0px", height: "30px" },
+          iconProps: {
+            style: { fontSize: "18px" },
+          },
+          href: "https://www.instagram.com/mitmsa/",
+          target: "_blank",
+        },
+        content: {
+          icon: InstagramIcon,
+        },
+      },
+
+      {
+        meta: {
+          isIcon: true,
+          style: { margin: "0 0 0 20px", height: "30px" },
+          iconProps: {
+            style: { fontSize: "18px" },
+          },
+          href: "https://www.facebook.com/mitmsa",
+          target: "_blank",
+        },
+        content: {
+          icon: FacebookIcon,
+        },
+      },
+    ],
+  };
+};
