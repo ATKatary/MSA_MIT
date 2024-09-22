57e13332a98d4ed69d37188760be2efab4da724f
Ahmed Zain <azain@mit.edu>
2024-09-22 04:38:03 -0400

    separate info into team and resources pages. todo: scuffed on mobile

--- frontend/src/components/content/nav.js
+++ frontend/src/components/content/nav.js
@@ -11,132 +11,134 @@ import { SECTIONS, THEME } from "../../constants";
 const DEFAULT = SECTIONS.HOME.TITLE;
 
 export const NAV_GC = (props) => {
-  const right = [];
-  const sections = [
-    {
-      name: SECTIONS.DONATE.TITLE,
-      href: "https://giving.mit.edu/form/#/",
-    },
-    {
-      name: SECTIONS.LIFE.TITLE,
-    },
-    {
-      name: SECTIONS.CONTACT.TITLE,
-    },
-    {
-      name: SECTIONS.INFO.TITLE,
-    }
-    // {
-    //   inPage: true,
-    //   name: SECTIONS.MAILING_LIST.TITLE,
-    // },
-    // {
-    //     name: SECTIONS.PROGRAMS.TITLE,
-    //     dropdown: true,
-    // },
-  ];
-  for (const section of sections) {
-    right.push({
-      meta: {
-        className: `public-sans`,
-      },
-      content: {
-        title: (
-          <Link
-            href={
-              section.href
-                ? section.href
-                : `/${section.inPage ? "#" : ""}${section.name}`
-            }
-            id={`nav-${section.name}`}
-            style={{
-              ...THEME.NAV.STYLE.BTN,
-              color: THEME.SECONDARY,
-              borderRadius: "5px",
-              margin: "0 20px 0 20px",
-            }}
-            className={"select-section mobile-margin"}
-          >
-            {section.icon ? (
-              <section.icon style={{ margin: "0 10px 0 0" }} />
-            ) : (
-              <></>
-            )}{" "}
-            {section.name.toUpperCase().replace("-", " ")}
-          </Link>
-        ),
-      },
-    });
-  }
-
-  const select = (section) => {
+    const right = [];
+    const sections = [
+        {
+            name: SECTIONS.DONATE.TITLE,
+            href: "https://giving.mit.edu/form/#/",
+        },
+        {
+            name: SECTIONS.LIFE.TITLE,
+        },
+        {
+            name: SECTIONS.CONTACT.TITLE,
+        },
+        // {
+        //     name: SECTIONS.INFO.TITLE,
+        // },
+        // {
+        //   inPage: true,
+        //   name: SECTIONS.MAILING_LIST.TITLE,
+        // },
+        // {
+        //     name: SECTIONS.PROGRAMS.TITLE,
+        //     dropdown: true,
+        // },
+        {
+            name: SECTIONS.TEAM.TITLE,
+        },
+        {
+            name: SECTIONS.RESOURCES.TITLE,
+        },
+    ];
     for (const section of sections) {
-      const sectionBtn = document.getElementById(`nav-${section.name}`);
-      if (sectionBtn) sectionBtn.classList.remove("select-section");
+        right.push({
+            meta: {
+                className: `public-sans`,
+            },
+            content: {
+                title: (
+                    <Link
+                        href={
+                            section.href
+                                ? section.href
+                                : `/${section.inPage ? "#" : ""}${section.name}`
+                        }
+                        id={`nav-${section.name}`}
+                        style={{
+                            ...THEME.NAV.STYLE.BTN,
+                            color: THEME.SECONDARY,
+                            borderRadius: "5px",
+                            margin: "0 20px 0 20px",
+                        }}
+                        className={"select-section mobile-margin"}
+                    >
+                        {section.icon ? <section.icon style={{ margin: "0 10px 0 0" }} /> : <></>}{" "}
+                        {section.name.toUpperCase().replace("-", " ")}
+                    </Link>
+                ),
+            },
+        });
     }
 
-    const sectionBtn = document.getElementById(section.id);
-    if (sectionBtn) {
-      props.setOpen(false);
-      props.setSelected(section.name);
-      sectionBtn.classList.add("select-section");
-    }
-  };
-  return {
-    NAV_LEFT: [
-      {
-        meta: {
-          href: "/",
-          id: `nav-${SECTIONS.HOME.TITLE}`,
-          style: {
-            margin: "0 0 10px 0",
-            display: "flex",
-            flexDirection: "column",
-          },
-        },
-        content: {
-          title: (
-            <img
-              src={logo}
-              height="50px"
-              className="pointer align-self-center logo-mobile"
-            />
-          ),
-        },
-      },
-    ],
+    const select = (section) => {
+        for (const section of sections) {
+            const sectionBtn = document.getElementById(`nav-${section.name}`);
+            if (sectionBtn) sectionBtn.classList.remove("select-section");
+        }
 
-    NAV_RIGHT: [
-      ...right,
-      {
-        meta: {
-          isIcon: true,
-          style: { margin: "0 10px 0 0px", height: "30px" },
-          iconProps: {
-            style: { fontSize: "18px" },
-          },
-          href: "https://www.instagram.com/mitmsa/",
-          target: "_blank",
-        },
-        content: {
-          icon: InstagramIcon,
-        },
-      },
+        const sectionBtn = document.getElementById(section.id);
+        if (sectionBtn) {
+            props.setOpen(false);
+            props.setSelected(section.name);
+            sectionBtn.classList.add("select-section");
+        }
+    };
+    return {
+        NAV_LEFT: [
+            {
+                meta: {
+                    href: "/",
+                    id: `nav-${SECTIONS.HOME.TITLE}`,
+                    style: {
+                        margin: "0 0 10px 0",
+                        display: "flex",
+                        flexDirection: "column",
+                    },
+                },
+                content: {
+                    title: (
+                        <img
+                            src={logo}
+                            height="50px"
+                            className="pointer align-self-center logo-mobile"
+                        />
+                    ),
+                },
+            },
+        ],
 
-      {
-        meta: {
-          isIcon: true,
-          style: { margin: "0 0 0 5px", height: "30px" },
-          iconProps: {
-            style: { fontSize: "18px" },
-          },
-          href: "https://www.facebook.com/mitmsa",
-          target: "_blank",
-        },
-        content: {
-          icon: FacebookIcon,
-        },
-      },
-    ],
-  };
+        NAV_RIGHT: [
+            ...right,
+            {
+                meta: {
+                    isIcon: true,
+                    style: { margin: "0 10px 0 0px", height: "30px" },
+                    iconProps: {
+                        style: { fontSize: "18px" },
+                    },
+                    href: "https://www.instagram.com/mitmsa/",
+                    target: "_blank",
+                },
+                content: {
+                    icon: InstagramIcon,
+                },
+            },
+
+            {
+                meta: {
+                    isIcon: true,
+                    style: { margin: "0 0 0 5px", height: "30px" },
+                    iconProps: {
+                        style: { fontSize: "18px" },
+                    },
+                    href: "https://www.facebook.com/mitmsa",
+                    target: "_blank",
+                },
+                content: {
+                    icon: FacebookIcon,
+                },
+            },
+        ],
+    };
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,8 @@ import Contact from "./pages/contact";
 import Life from "./pages/life";
 import Landing from "./pages/landing";
 import Info from "./pages/info";
+import Team from "./pages/team";
+import Resources from "./pages/resources";
 import { SECTIONS } from "./constants";
 
 const root = ReactDOM.createRoot(document.getElementById("appRoot"));
@@ -21,7 +23,9 @@ root.render(
         <Route path={`/${SECTIONS.LIFE.TITLE}`} element={<Life />} />
         <Route path={`/${SECTIONS.DONATE.TITLE}`} element={<Landing />} />
         <Route path={`/${SECTIONS.CONTACT.TITLE}`} element={<Contact />} />
-        <Route path={`/${SECTIONS.INFO.TITLE}`} element={<Info />} />
+        {/* <Route path={`/${SECTIONS.INFO.TITLE}`} element={<Info />} /> */}
+        <Route path={`/${SECTIONS.TEAM.TITLE}`} element={<Team />} />
+        <Route path={`/${SECTIONS.RESOURCES.TITLE}`} element={<Resources />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
